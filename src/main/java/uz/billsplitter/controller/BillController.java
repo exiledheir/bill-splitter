f143ae876dd2ed98119e9db4921627b70cc717b4
mukhammadjonkholmukhamedov <kh.muxammadjon2@gmail.com>
2025-12-10 16:59:47 +0500

    fixed annotations

--- src/main/java/uz/billsplitter/controller/BillController.java
+++ src/main/java/uz/billsplitter/controller/BillController.java
@@ -1,7 +1,11 @@
 package uz.billsplitter.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
 public class BillController {
 }
--- src/main/java/uz/billsplitter/service/impl/BillServiceImpl.java
+++ src/main/java/uz/billsplitter/service/impl/BillServiceImpl.java
@@ -1,8 +1,10 @@
 package uz.billsplitter.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uz.billsplitter.service.BillService;
 
 @Service
+@RequiredArgsConstructor
 public class BillServiceImpl implements BillService {
 }
